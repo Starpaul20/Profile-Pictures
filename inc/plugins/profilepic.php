31a64d07b78599e7779b9f18bdc4d6ed47d849eb
Paul Bender <paulbender@live.com>
2021-06-27 18:24:46 -0400

    Bump to version v1.4

--- inc/plugins/profilepic.php
+++ inc/plugins/profilepic.php
@@ -95,7 +95,7 @@ function profilepic_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.3",
+		"version"			=> "1.4",
 		"codename"			=> "profilepic",
 		"compatibility"		=> "18*"
 	);
